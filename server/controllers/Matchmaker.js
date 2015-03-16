c74077c6591f48521eed44f7f37cef94900d983e
Andreas Drivenes <andreas.drivenes@gmail.com>
2015-03-16 13:01:25 +0100

    Send game object back to user. Update API description

--- server/controllers/Matchmaker.js
+++ server/controllers/Matchmaker.js
@@ -10,13 +10,14 @@ exports.startGame = function(req, res) {
 
   // player has ongoing game
   if (game != null) {
-    res.json(game);
+    res.json('message': 'Ongoing game', 'game': game);
   }
 
   // Start a new game if opponent exists and opponent is not the same player. Pops the last element in queue.
   else if (playerQueue.length > 0 && playerQueue.indexOf(player1) === -1) {
     OngoingGames.startGame(player1, playerQueue.pop());
-    res.json({'message': 'Starting new game.'});
+    game = OngoingGames.findGame(player1);
+    res.json({'message': 'Starting new game.', 'game': game});
   }
 
   // If no players in queue, put this player in start of queue.
