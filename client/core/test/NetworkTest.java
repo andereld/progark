65dcbf46e385a121cd1862fc7c4b612c6a8034e5
Andreas Drivenes <andreas.drivenes@gmail.com>
2015-03-18 21:37:34 +0100

    Add test to check that the opponent is set correctly

--- client/core/test/NetworkTest.java
+++ client/core/test/NetworkTest.java
@@ -4,19 +4,27 @@
 
 
 import org.junit.Test;
+import org.junit.Before;
 import static org.junit.Assert.*;
 
 import com.mygdx.game.controllers.GameNetworkController;
-import com.mygdx.game.controllers.PlayerNetworkController;
 
 import java.util.concurrent.TimeUnit;
 
 
 public class NetworkTest {
+
+    private GameNetworkController game1;
+    private GameNetworkController game2;
+
+    @Before
+    public void setup() {
+        game1 = new GameNetworkController();
+        game2 = new GameNetworkController();
+    }
+
     @Test
     public void testGameInitialization() throws InterruptedException {
-        GameNetworkController game1 = new GameNetworkController();
-        GameNetworkController game2 = new GameNetworkController();
 
         game1.startGame("andybb");
         assertEquals("Username for player 1 given and in model should be equal ", "andybb", game1.getPlayerController().getPlayer().getUsername());
@@ -24,6 +32,11 @@ public class NetworkTest {
         TimeUnit.SECONDS.sleep(1);
         game2.startGame("mats");
         assertEquals("Username for player 2 given and in model should be equal ", "mats", game2.getPlayerController().getPlayer().getUsername());
+
+        TimeUnit.SECONDS.sleep(11);
+        assertEquals("The opponent of player 1 should player 2", "mats", game1.getPlayerController().getOpponent().getUsername());
+        assertEquals("The opponent of player 2 should player 1", "andybb", game2.getPlayerController().getOpponent().getUsername());
+
 
     }
 
