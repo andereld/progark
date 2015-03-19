c89a2367c936162bbcba0a9388a34e6b1d1f7334
Mats Byrkjeland <matsbyr@gmail.com>
2015-03-19 13:31:44 +0100

    Fix require in GameController

--- server/controllers/GameController.js
+++ server/controllers/GameController.js
@@ -1,17 +1,17 @@
-var Game = require('../models/Game');
+var models = require('../models');
 
 /*
  * Find array of all games in database that are not finished
  */
 exports.findOngoingGames = function() {
-  return Game.findAll({ where: { finished: false } });
+  return models.Game.findAll({ where: { finished: false } });
 };
 
 /*
  * Find game belonging to player
  */
 exports.findGame = function(player) {
-  return findOngoingGames().filter(function(element, index, array) {
+  return exports.findOngoingGames().filter(function(element, index, array) {
     return element.player1 === player || element.player2 === player;
   })[0];
 };
@@ -20,11 +20,11 @@ exports.findGame = function(player) {
  * Start a new game for two players.
  */
 exports.startGame = function(player1, player2) {
-  Game.create({
+  models.Game.create({
     player1: player1,
     player2: player2,
     next: player1,
-    board1: Board.classMethods.randomBoard(),
-    board2: Board.classMethods.randomBoard()
+    board1: models.Board.classMethods.randomBoard(),
+    board2: models.Board.classMethods.randomBoard()
   });
 };
