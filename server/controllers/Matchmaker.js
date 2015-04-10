d1f620b149255d66204a4683e57e87bc337a2045
Anders Wold Eldhuset <andereld@samfundet.no>
2015-04-10 15:54:20 +0200

    Make the startGame action return the newly created game

--- server/controllers/Matchmaker.js
+++ server/controllers/Matchmaker.js
@@ -14,9 +14,9 @@ exports.startGame = function(req, res) {
 
     // Start a new game if opponent exists and opponent is not the same player. Pops the last element in queue.
     else if (playerQueue.length > 0 && playerQueue.indexOf(player1) === -1) {
-      GameController.startGame(player1, playerQueue.pop());
-      game = GameController.findGame(player1);
-      res.json({'game': game});
+      GameController.startGame(player1, playerQueue.pop()).then(function(game) {
+        res.json({'game': game});
+      });
     }
 
     // If no players in queue, put this player in start of queue.
