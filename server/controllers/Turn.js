274ec30068d69c74b3c6b70104a7a887e8e14dd5
Anders Wold Eldhuset <andereld@samfundet.no>
2015-04-13 16:09:21 +0200

    Include coordinates of last move in /api/turn response
    Include the coordinates of the last cell which was fired upon in the
    response to /api/turn requests so that the client can update its local
    board.

--- server/controllers/Turn.js
+++ server/controllers/Turn.js
@@ -5,7 +5,7 @@ exports.nextPlayer = function(req, res) {
 
   GameController.findGame(player).then(function(game) {
     if (game != null) {
-      res.json({username: game.next});
+      res.json({username: game.next, lastMove: game.lastMove});
     } else {
       res.json({message: 'No game was found.'});
     }
