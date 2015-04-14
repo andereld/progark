9043366344e1c4b2a45457c201cdbe5142465a41
Anders Wold Eldhuset <andereld@samfundet.no>
2015-04-14 15:30:39 +0200

    Fix bug which caused too many ships in randomly generated boards
    generator/Levels.js used a global variable to hold a randomly generated
    board. This variable was reused without being cleared inbetween function
    calls, resulting in the number of cells occupied by ships doubling for
    each function call.
    
    This commit fixes that bug and removes the global state.

--- server/generator/Levels.js
+++ server/generator/Levels.js
@@ -1,24 +1,12 @@
-
-// setup a 10x10 array
-var board = new Array(10);
-var ships = [[2,3], [1,4], [1,3], [1,3], [1,2]];
-
-for(var i = 0; i < 10; i++) {
-  board[i] = [];
-  for(var j = 0; j < 10; j++) {
-    board[i][j] = false;
-  }
-}
-
 // parses a 10x10 array to a 100 char string
 exports.getRandomBoardAsString = function() {
-  var board = generateRandomBoard();
+  var board = generateRandomBoard(10);
   var boardString = "";
-  for(var i = 0; i < board.length; i++) {
+  for (var i = 0; i < board.length; i++) {
     var row = board[i];
 
-    for(var j = 0; j < board.length; j++) {
-      if(row[j] == true) {
+    for (var j = 0; j < board.length; j++) {
+      if (row[j] == true) {
         boardString += "S";
       }
       else {
@@ -29,57 +17,75 @@ exports.getRandomBoardAsString = function() {
   return boardString;
 };
 
-// generates a random 10x10 board with ships placed either horizontally or vertically
-var generateRandomBoard = function() {
-  for(var i = 0; i < ships.length; i++) {
+// Generates an n-by-n array with all values set to false.
+var generateEmptyBoard = function(lengthOfSides) {
+  var board = new Array(lengthOfSides);
 
-    var placed = false;
-
-    while(!placed) {
-      var ship = ships[i];
-      var x = Math.floor((Math.random() * 10));
-      var y = Math.floor((Math.random() * 10));
-      var orientation = Math.random() > 0.5 ? 'hori' : 'verti';
-      placed =  placeShip(ship, x, y, orientation);
-
+  for (var i = 0; i < lengthOfSides; i++) {
+    board[i] = new Array(lengthOfSides);
+    for (var j = 0; j < lengthOfSides; j++) {
+      board[i][j] = false;
     }
   }
+
   return board;
 };
 
-// helper method to check that a ship can be placed at the given coordinates
-var canPlace = function(ship, x, y, orientation) {
+// Generates an n-by-n (n >= 5) board with five ships placed randomly
+// (both horizontally and vertically).
+var generateRandomBoard = function(lengthOfSides) {
+  var board = generateEmptyBoard(lengthOfSides);
+  var ships = [[2,3], [1,4], [1,3], [1,3], [1,2]];
+
+  for (var i = 0; i < ships.length; i++) {
+    var placed = false;
+
+    while (!placed) {
+      var ship = ships[i];
+      var x = Math.floor((Math.random() * lengthOfSides));
+      var y = Math.floor((Math.random() * lengthOfSides));
+      var orientation = Math.random() > 0.5 ? 'hori' : 'verti';
+      placed = tryToPlaceShip(ship, board, x, y, orientation);
+    }
+  }
+
+  return board;
+};
+
+// Helper method to check that a ship can be placed at the given coordinates.
+var canPlace = function(ship, board, x, y, orientation) {
   var length = orientation == 'hori' ? ship[1] : ship[0];
   var height = orientation == 'hori' ? ship[0] : ship[1];
 
-  if((x + length) > board[0].length || (y + height) > board.length) {
+  if ((x + length) > board[0].length || (y + height) > board.length) {
     return false;
   }
 
-  for(var i = y; i < y + height; i++) {
-    for(var j = x; j < x + length; j++) {
-      if(board[i][j] == true) {
+  for (var i = y; i < y + height; i++) {
+    for (var j = x; j < x + length; j++) {
+      if (board[i][j] == true) {
         return false;
       }
     }
   }
+
   return true;
 };
 
-// helper method to place a ship at the given coordinates
-var placeShip = function(ship, x, y, orientation) {
+// Helper method to place a ship at the given coordinates.
+var tryToPlaceShip = function(ship, board, x, y, orientation) {
   var length = orientation == 'hori' ? ship[1] : ship[0];
   var height = orientation == 'hori' ? ship[0] : ship[1];
 
-  if(canPlace(ship, x, y, orientation)) {
-    for(var i = y; i < y + height; i++) {
-      for(var j = x; j < x + length; j++) {
+  if (canPlace(ship, board, x, y, orientation)) {
+    for (var i = y; i < y + height; i++) {
+      for (var j = x; j < x + length; j++) {
         board[i][j] = true;
       }
     }
+
     return true;
   }
+
   return false;
-
 };
-
