a9cab4d50202c231a9c8f49547d0574289cca2b7
Andreas Drivenes <andreas.drivenes@gmail.com>
2015-04-20 11:25:33 +0200

    Change request time to 5000 ms

--- client/core/src/com/mygdx/game/Constants.java
+++ client/core/src/com/mygdx/game/Constants.java
@@ -4,5 +4,5 @@ package com.mygdx.game;
  * Created by esso on 18.03.15.
  */
 public class Constants {
-    public static final int REGULAR_REQUEST_TIME = 10000;
+    public static final int REGULAR_REQUEST_TIME = 5000;
 }
