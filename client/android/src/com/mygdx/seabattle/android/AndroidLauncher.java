29cb83f2e7935622f02ca2b4856999050c05a1ce
Andreas Drivenes <andreas.drivenes@gmail.com>
2015-04-21 02:51:33 +0200

    Start on a Notification service

--- client/android/src/com/mygdx/seabattle/android/AndroidLauncher.java
+++ client/android/src/com/mygdx/seabattle/android/AndroidLauncher.java
@@ -1,16 +1,32 @@
 package com.mygdx.seabattle.android;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.seabattle.AndroidNotification;
 import com.mygdx.seabattle.SeaBattle;
 
-public class AndroidLauncher extends AndroidApplication {
+public class AndroidLauncher extends AndroidApplication implements AndroidNotification {
+	private Intent intent;
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new SeaBattle(), config);
+		initialize(new SeaBattle(this), config);
+	}
+
+	@Override
+	public void invokeService(String username) {
+		intent = new Intent(this, SeaBNotificationService.class);
+		intent.putExtra("username", username);
+		startService(intent);
+
+	}
+
+	@Override
+	public void purgeService() {
+		stopService(intent);
 	}
 }
--- client/desktop/src/com/mygdx/seabattle/desktop/DesktopLauncher.java
+++ client/desktop/src/com/mygdx/seabattle/desktop/DesktopLauncher.java
@@ -2,13 +2,28 @@ package com.mygdx.seabattle.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.seabattle.AndroidNotification;
 import com.mygdx.seabattle.SeaBattle;
 
-public class DesktopLauncher {
+public class DesktopLauncher implements AndroidNotification {
+	private static DesktopLauncher app;
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = SeaBattle.VIRTUAL_WIDTH;
 		config.height = SeaBattle.VIRTUAL_HEIGHT;
-		new LwjglApplication(new SeaBattle(), config);
+		if(app == null) {
+			app = new DesktopLauncher();
+		}
+		new LwjglApplication(new SeaBattle(app), config);
+	}
+
+	@Override
+	public void invokeService(String username) {
+
+	}
+
+	@Override
+	public void purgeService() {
+
 	}
 }
